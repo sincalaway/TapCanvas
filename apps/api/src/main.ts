02efb91e3b550c6ddac7f3e1dc64018d5168ef5d
李碧强 <624144061@qq.com>
2025-11-18 19:37:50 +0800

    允许跨域

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,16 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: [
-      'https://tapcanvas.beqlee.icu',
-      'https://tapcanvas-api.beqlee.icu',
-      'http://localhost:5173',
-      'http://localhost:5174',
-      'http://127.0.0.1:5173',
-      'http://127.0.0.1:5174',
-      // 开发环境允许所有本地地址
-      /^https?:\/\/(localhost|127\.0\.0\.1)(:\d+)?$/
-    ],
+    origin: true,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Accept']
